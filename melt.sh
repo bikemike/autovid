127fe3ab790e937ea1316c0c0eec322fbf8fd67a
Mike Morrison <bikemike@gmail.com>
2018-02-04 00:24:09 -0800

    fix ken burns effect and add face detection for images

--- melt.sh
+++ melt.sh
@@ -4,5 +4,5 @@ for file in $*
 do
 	echo PROCESSING $file
 	echo "melt xml:$file -progress -consumer avformat:${file%.*}.avi acodec=libmp3lame vcodec=libx264 threads=8"
-	melt xml:$file -progress -consumer avformat:${file%.*}.avi acodec=libmp3lame vcodec=libx264 threads=8
+	melt xml:$file -profile hdv_1080_30p -progress -consumer avformat:${file%.*}.avi rescale=bicubic acodec=libmp3lame vcodec=libx264 threads=8
 done
